3ded20e82a48b99824132dbf97819ba33aad8c14
Quayyum-a <user@Quayyums-MacBook-Air.local>
2026-08-01 12:50:41 +0100

    Add Permissions-Policy header configuration

--- Papyr/next.config.js
+++ Papyr/next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  headers: async () => {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            value: 'geolocation=(), microphone=(), camera=()',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
